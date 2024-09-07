f9bab049b0d585527f18cb2bc3afb48f07961b56
André Souza <98634615+Andrelslp@users.noreply.github.com>
2024-09-07 19:15:56 -0300

    fix: build error

--- src/components/NavbarItem.tsx
+++ src/components/NavbarItem.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Suspense } from 'react';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 
@@ -13,13 +14,15 @@ export default function NavbarItem({ title, param }: NavbarItemProps) {
   const genre = searchParams.get('genre');
 
   return (
-    <div>
-      <Link
-        className={`hover:text-amber-600 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg' : ''}`}
-        href={`/?genre=${param}`}
-      >
-        {title}
-      </Link>
-    </div>
+    <Suspense>
+      <div>
+        <Link
+          className={`hover:text-amber-600 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg' : ''}`}
+          href={`/?genre=${param}`}
+        >
+          {title}
+        </Link>
+      </div>
+    </Suspense>
   );
 }
