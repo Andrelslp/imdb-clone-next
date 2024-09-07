532feba25f17347e834d1f6f6bce5b2bb878b218
André Souza <98634615+Andrelslp@users.noreply.github.com>
2024-09-07 15:06:29 -0300

    feat: add card component

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,3 +1,4 @@
+import Card from './Card';
 import { TheMovieDbProps } from '@/app/page';
 
 interface ResultsProps {
@@ -7,11 +8,9 @@ interface ResultsProps {
 export default function Results({ results }: ResultsProps) {
   console.log(results)
   return (
-    <div>
+    <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4'>
       {results.map((result) => (
-        <div key={result.id}>
-          <h2>{result.title || result.name}</h2>
-        </div>
+        <Card key={result.id} result={result} />
       ))}
     </div>
   );
